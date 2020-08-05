23e4063b138b7ea28aa3ff108855f1116a844d59
Natalia Beckstead <toosmiravilla@gmail.com>
2020-08-04 22:09:34 -0600

    Met MVP

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Route, Switch } from "react-router-dom";
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
+import Hi from './Movies/Hi'
+
+import SavedList from './Movies/SavedList';
+
+const App = () => {
+  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const getMovies = () => {
+      axios
+        .get('http://localhost:5000/api/movies')
+        .then(response => {
+          console.log(response.data);
+          setMovieList(response.data);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    }
+    getMovies();
+  }, []);
+
+  const addToSavedList = id => {
+    setSaved( [...saved, movieList[id]] );
+  };
+
+  return (
+    <div>
+      {/* <SavedList list={[ ]} /> */}
+      <Switch>
+        <Route path="/movie/:id">
+          <Movie />
+        </Route>
+
+        <Route
+          exact
+          path="/"
+          render={() => 
+            <MovieList movies={movieList}/>
+        }/>
+      </Switch>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/Movies/MovieList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const MovieList = props => {
+  return (
+    <div className="movie-list">
+      {props.movies.map(movie => (
+        <Link to={`/movie/${movie.id}`}>
+          <MovieDetails key={movie.id} movie={movie} />
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+function MovieDetails({ movie }) {
+  const { title, director, metascore } = movie;
+  return (
+    
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+      </div>
+    
+  );
+}
+
+export default MovieList;
